9cca15292d2acc377ead7fb7fee2b0ee8396a74c
Robert23e <145117391+Robert23e@users.noreply.github.com>
2023-11-09 16:26:00 +0600

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,9 @@
+apt update -y
+pkg install toilet
+pkg inatall cowsay
+https://github.com/Robert23e/Banner-.git
+cd Termux-Banner
+chmod +x requirement.sh
+chmod +x t-ban.sh
+bash requirement.sh
+bash t-ban.sh
